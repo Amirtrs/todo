634260dbb267be2c98affeea2d75b53c977d83fe
amirtrs <amir.toranj21@gmail.com>
2020-04-28 08:44:26 +0430

    Updates

---
+++ precache-manifest.ca8d415e75ec247737f1bfb917fe28ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd7ec425f502b023ace6332bffb324c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c79744fa15d33e5bbd4f",
+    "url": "/todo/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "c79744fa15d33e5bbd4f",
+    "url": "/todo/static/js/2.8790895a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo/static/js/2.8790895a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa1b117292323116773",
+    "url": "/todo/static/js/main.117794c2.chunk.js"
+  },
+  {
+    "revision": "c8eac0261f9f307a1e88",
+    "url": "/todo/static/js/runtime-main.d5e4d962.js"
+  }
+]);
